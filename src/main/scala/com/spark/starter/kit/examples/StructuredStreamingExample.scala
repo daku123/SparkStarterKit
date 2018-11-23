f27abb6d7b33ddb48467c400c3d1ed0cfc8987ce
vishu <vishudadhich001@gmail.com>
2018-11-23 12:27:25 +0530

    streaming code added

--- src/main/scala/com/spark/starter/kit/examples/StructuredStreamingExample.scala
+++ src/main/scala/com/spark/starter/kit/examples/StructuredStreamingExample.scala
@@ -2,6 +2,7 @@ package com.spark.starter.kit.examples
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StructType
 
 
 object StructuredStreamingExample {
@@ -13,20 +14,56 @@ object StructuredStreamingExample {
       .config("spark.sql.warehouse.dir", "/user/hive/warehouse")
       .config("spark.master","local")
       .getOrCreate()
+     import spark.implicits._
+     spark.conf.set("spark.sql.shuffle.partitions", "5")
 
     val retailData = spark.read.option("inferschema","true")
       .option("header","true")
       .csv("C:\\Users\\vishunath.sharma\\github\\Spark-The-Definitive-Guide\\data\\retail-data\\by-day\\*.csv")
-    import spark.implicits._
+
+
 
     retailData.createGlobalTempView("retail_data")
      val retailSchema = retailData.schema
-    retailData.selectExpr("CustomerId","(UnitPrice*Quantity) as total_cost","InvoiceDate").groupBy(col("CustomerId"),window(col("InvoiceDate"),"1 day"))
+
+    /*retailData.selectExpr("CustomerId","(UnitPrice*Quantity) as total_cost","InvoiceDate").groupBy(col("CustomerId"),window(col("InvoiceDate"),"1 day"))
       .sum("total_cost").show(5)
+
 
     retailData.selectExpr("CustomerId","(UnitPrice*Quantity) as total_cost","InvoiceDate","UnitPrice","Quantity")
               .groupBy(col("CustomerId"),window(col("InvoiceDate"),"1 day"))
               .agg(max(col("UnitPrice")),min("Quantity"))
               .show(5)
+*/
+  /*
+  read the data as streaming. The above code is for batch data.
+   */
+ streamingData(spark,retailSchema)
+  }
+
+  def streamingData(spark:SparkSession,staticSchema:StructType)={
+
+    val realTimeStreamingData = spark.readStream
+      .schema(staticSchema)
+      .option("inferschema","true")
+      .option("header","true")
+      .option("maxFilesPerTrigger","1")
+      .csv("C:\\Users\\vishunath.sharma\\github\\Spark-The-Definitive-Guide\\data\\retail-data\\by-day\\*.csv")
+
+     realTimeStreamingData.isStreaming
+
+     val purchaseByCustomerPerHour = realTimeStreamingData
+                                     .selectExpr("CustomerId","(UnitPrice*Quantity) as total_cost","InvoiceDate")
+                                     .groupBy(col("CustomerId"),window(col("InvoiceDate"),"1 day"))
+                                     .sum("total_cost")
+
+     purchaseByCustomerPerHour
+                              .writeStream.format("memory")
+                              .queryName("customer_purchases")
+                              .outputMode("complete")
+                              .start()
+
+
+
   }
 }
