73f54960ce8f5dd6d5689f1052bc9ef636d5dca6
vishu <vishudadhich001@gmail.com>
2018-11-30 12:43:39 +0530

    dataframe operations

--- src/main/scala/com/spark/starter/kit/examples/DataFrame.scala
+++ src/main/scala/com/spark/starter/kit/examples/DataFrame.scala
@@ -1,6 +1,6 @@
 package com.spark.starter.kit.examples
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
@@ -15,20 +15,16 @@ object DataFrame  {
       .config("spark.master", "local")
       .getOrCreate()
 
-    import spark.implicits._
-    val authors = Seq("bill,databricks", "matei,databricks")
-    authors.toDF("colmn1").show()
+    spark.sparkContext.setLogLevel("ERROR")
 
-    usingDataFrameApi(spark)
-    readJsonUsingDataFrame(spark)
+    dataFrameOperations(spark)
 
   }
 
   def usingDataFrameApi(spark:SparkSession)={
     val flightData = spark.read.option("inferschema","true").option("header","true").csv("C:\\Users\\vishunath.sharma\\github\\Spark-The-Definitive-Guide\\data\\flight-data\\csv")
-    /*
-   creating a temp table on flightData.
-   */
+
+   //creating a temp table on flightData.
     flightData.createOrReplaceTempView("flight_data_2010")
     flightData.select( max("count")).show()
     flightData.groupBy("DEST_COUNTRY_NAME")
@@ -46,17 +42,15 @@ object DataFrame  {
 
     val jsonDataFrame =  spark.read
                         .format("json")
-                        .load("C:\\Users\\vishunath.sharma\\github\\Spark-The-Definitive-Guide\\data\\flight-data\\json\\2015-summary.json")
+                        .load("C:\\Users\\vishunath.sharma\\github\\" +
+                          "Spark-The-Definitive-Guide\\data\\flight-data\\json\\" +
+                          "2015-summary.json")
     jsonDataFrame.printSchema()
-    //jsonDataFrame.schema
 
-    /*
-    creating manual schema for the given dataset
-    */
-
+    //creating manual schema for the given dataset
     val manualSchema = StructType(List(StructField("DEST_COUNTRY_NAME",StringType,true),
                        StructField("ORIGIN_COUNTRY_NAME",StringType,true),
-                       StructField("Count",LongType,true,Metadata.fromJson("{\"hello \":\"world\"}"))))
+                       StructField("count",LongType,true,Metadata.fromJson("{\"hello \":\"world\"}"))))
 
     val dfWithSchema = spark.read
                        .format("json")
@@ -64,5 +58,66 @@ object DataFrame  {
                        .load("C:\\Users\\vishunath.sharma\\github\\Spark-The-Definitive-Guide\\data\\flight-data\\json\\2015-summary.json")
 
      dfWithSchema.limit(4).show()
+  }
+
+  def createDataFrameFromRows(spark:SparkSession)={
+    val rows = Seq(Row("India","US",1202L),Row("India","Russia",334L))
+    val rdd = spark.sparkContext.parallelize(rows)
+    val dataFrame = spark.createDataFrame(rdd,createManualSchema())
+    dataFrame.show()
+  }
+
+  def createManualSchema():StructType={
+    val manualSchema =  StructType(List(StructField("DEST_COUNTRY_NAME",StringType,true),
+       StructField("ORIGIN_COUNTRY_NAME",StringType,true),
+       StructField("count",IntegerType,false)))
+
+    return manualSchema
+  }
+
+  def dataFrameOperations(spark:SparkSession)={
+    val dfWithSchema = spark.read
+      .format("json")
+      .schema(createManualSchema())
+      .load("C:\\Users\\vishunath.sharma\\" +
+        "github\\Spark-The-Definitive-Guide\\data\\" +
+        "flight-data\\json\\2015-summary.json")
+
+     dfWithSchema.printSchema()
+    dfWithSchema.show(1)
+
+     dfWithSchema
+                 .selectExpr("ORIGIN_COUNTRY_NAME","(ORIGIN_COUNTRY_NAME=DEST_COUNTRY_NAME) as flag")
+                 .show(3)
+
+
+    //Adding a column to existing schema
+    val dfWithExtraColumn = dfWithSchema
+                            .withColumn("flag",expr("ORIGIN_COUNTRY_NAME=DEST_COUNTRY_NAME"))
+
+
+    dfWithExtraColumn.printSchema()
+
+    //casting of column
+    dfWithSchema.withColumn("count2",exp(col("count"))
+                .cast(StringType)).show(4)
+
+
+    // removing column and applying filter to dataframe.
+    val dfWithDroppedColumn = dfWithExtraColumn.drop("count2")
+    dfWithDroppedColumn.printSchema()
+
+    dfWithExtraColumn.where((col("Count").isNotNull)
+                       .and(col("DEST_COUNTRY_NAME")
+                         .equalTo("United States")))
+                     .show(10)
+
+  }
+
+  def readJson(spark:SparkSession)={
+    val data = spark.read.json("C:\\Users\\vishunath.sharma\\github" +
+                                      "\\Spark-The-Definitive-Guide\\data\\" +
+                                      "flight-data\\json\\2015-summary.json")
+    data.show(4)
   }
 }
